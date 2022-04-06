20f03ad0d67a6efae311167df42a03d0d5e89a4e
Zandermann1 <101841690+Zandermann1@users.noreply.github.com>
2022-04-05 20:13:55 -0500

    working on graffiti

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 // Select some elements...
-let header = document.querySelector(#page-header")
+let header = document.querySelector ("#page-header")
 header.style.textAlign = "left"
 
 let dogImages = document.querySelectorAll(".dog-image")
+for(let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRaidus 
+}
